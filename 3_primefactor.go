90c364ebaa88b6cc3ac95bcbd9f4f745d3386ab5
Mantej Rajpal <mantej@gmail.com>
2023-12-23 11:12:39 -0800

    project euler 3

---
+++ 3_primefactor.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func GetPrimeFactors(n int) []int {
+	var primes []int
+	var i int
+	sqrt := int(math.Sqrt(float64(n)))
+
+	for {
+		if n%2 == 0 {
+			n = n / 2
+			primes = append(primes, 2)
+		} else {
+			break
+		}
+	}
+
+	for i = 3; i < sqrt; i += 2 {
+		for n%i == 0 {
+			n = n / i
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
+
+func main() {
+	primes := GetPrimeFactors(600851475143)
+	fmt.Println(primes[len(primes)-1])
+}
