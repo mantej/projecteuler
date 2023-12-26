b17bb898b73ea6dc329914bfd9e7d5ef330d7ca3
Mantej Rajpal <mantej@gmail.com>
2023-12-25 23:41:31 -0800

    10001st prime

---
+++ 7_primes.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type primes struct {
+	value, position int
+}
+
+func NewPrimes() *primes {
+	return &primes{
+		value:    3,
+		position: 2,
+	}
+}
+
+func (p *primes) NextPrime() {
+	var prime bool
+	for i := p.value + 2; ; i += 2 {
+		prime = true
+		for j := 3; j < i; j += 2 {
+			if i%j == 0 {
+				prime = false
+				break
+			}
+		}
+		if prime {
+			p.value = i
+			p.position = p.position + 1
+			return
+		}
+	}
+}
+
+func main() {
+	p := NewPrimes()
+
+	for p.position < 10001 {
+		p.NextPrime()
+	}
+
+	fmt.Println(p.value)
+}
